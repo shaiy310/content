bc3e7f8065ff3b9a567134374fe3c3614be6796c
esharf <esahrf@paloaltonetworks.com>
2020-04-23 13:14:21 +0300

    fixing Vulture errors

---
+++ Packs/McAfee_ESM_v2/Integrations/McAfee_ESM_v2/whitelist.py
@@ -0,0 +1,5 @@
+# pylint: skip-file
+# flake8: noqa
+# type: ignore
+_is_status_code_valid  # unused function (Packs/McAfee_ESM_v2/Integrations/McAfee_ESM_v2/McAfee_ESM_v2.py:39)
+other  # unused variable (Packs/McAfee_ESM_v2/Integrations/McAfee_ESM_v2/McAfee_ESM_v2.py:39)
